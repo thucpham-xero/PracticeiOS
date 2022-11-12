53950e2676ca8ed84d6b250e118d505722816cab
Thuc Pham <thuc.pham@xero.com>
2022-11-12 23:51:39 +1100

    Add xcode project

---
+++ PracticeiOS/PracticeiOS/PracticeiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeiOSApp.swift
+//  PracticeiOS
+//
+//  Created by Thuc Pham on 12/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct PracticeiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
